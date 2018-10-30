bcec5e296a11e74ea29b77cb2ff192d73df5a0c5
Warren Hoburg <whoburg@mit.edu>
2018-10-29 21:54:14 -0500

    rename safe to anonfeedback

--- anonfeedback/anonfeedback/wsgi.py
+++ anonfeedback/anonfeedback/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for safe project.
+WSGI config for anonfeedback project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anonfeedback.settings')
 
 application = get_wsgi_application()
