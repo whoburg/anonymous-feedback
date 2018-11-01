c3d899f51a8959106c34e38c8ed83b46368a9659
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:00:17 -0500

    new Survey model to contain Questions

--- feedbacksite/surveys/views.py
+++ feedbacksite/surveys/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Question
+from .models import Survey
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    latest_survey_list = Survey.objects.order_by('pub_date')[:5]
+    context = {'latest_survey_list': latest_survey_list}
     return render(request, 'surveys/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, survey_id):
+    return HttpResponse("You're looking at survey %s." % survey_id)
