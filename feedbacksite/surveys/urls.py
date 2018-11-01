0524c813afea93ed8aa8c264b0598b4c5be10fae
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:42:50 -0500

    first form, questions are radio buttons but will change

--- feedbacksite/surveys/urls.py
+++ feedbacksite/surveys/urls.py
@@ -6,4 +6,6 @@ app_name = 'surveys'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:survey_id>/', views.detail, name='detail'),
+    path('<int:survey_id>/submit/', views.submit, name='submit'),
+    path('<int:survey_id>/submitted/', views.submitted, name='submitted'),
 ]
