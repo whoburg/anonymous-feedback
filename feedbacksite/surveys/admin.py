9aba4c1b13e18e9234c57e5cedd45ddd456c9c01
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:15:43 -0500

    Add questions to admin page

--- feedbacksite/surveys/admin.py
+++ feedbacksite/surveys/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
-from .models import Survey
+from .models import Survey, Question
 
-admin.site.register(Survey)
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['survey_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [QuestionInline]
+
+admin.site.register(Survey, SurveyAdmin)
