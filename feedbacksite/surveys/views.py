6e0ba6e89b750a79f283b2522fce737a5761668c
Warren Hoburg <whoburg@mit.edu>
2018-11-01 17:11:24 -0500

    basic view listing Question objects

--- feedbacksite/surveys/views.py
+++ feedbacksite/surveys/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the surveys index.")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
