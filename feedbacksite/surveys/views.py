89be216bb6fe3d606b440c280cf21d9a5eee3656
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:42:50 -0500

    first form, questions are radio buttons but will change

--- feedbacksite/surveys/views.py
+++ feedbacksite/surveys/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Survey
+from .models import Survey, Question
 
 
 def index(request):
@@ -11,3 +13,24 @@ def index(request):
 def detail(request, survey_id):
     survey = get_object_or_404(Survey, pk=survey_id)
     return render(request, 'surveys/detail.html', {'survey': survey})
+
+def submit(request, survey_id):
+    survey = get_object_or_404(Survey, pk=survey_id)
+    try:
+        selected_question = survey.question_set.get(pk=request.POST['question'])
+    except (KeyError, Question.DoesNotExist):
+        # Redisplay the survey voting form.
+        return render(request, 'surveys/detail.html', {
+            'survey': survey,
+            'error_message': "You didn't select a question.",
+        })
+    else:
+        selected_question.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('surveys:submitted', args=(survey.id,)))
+
+def submitted(request, survey_id):
+    survey = get_object_or_404(Survey, pk=survey_id)
+    return render(request, 'surveys/submitted.html', {'survey': survey})
