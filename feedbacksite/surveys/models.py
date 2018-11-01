b0b32b2c832815034a5c7872d486f255b8401548
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:00:17 -0500

    new Survey model to contain Questions

--- feedbacksite/surveys/models.py
+++ feedbacksite/surveys/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Survey(models.Model):
+    survey_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question_text
+        return self.survey_text
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
