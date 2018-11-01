0c50d69505c95a9462a1d36db74bced34d715e96
Warren Hoburg <whoburg@mit.edu>
2018-11-01 18:24:09 -0500

    list questions on detail view, better use of Django templates

--- feedbacksite/surveys/urls.py
+++ feedbacksite/surveys/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'surveys'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:survey_id>/', views.detail, name='detail'),
