b01ae659b23f1f7715f8404f6a8219a999bd7d81
Warren Hoburg <whoburg@mit.edu>
2018-11-02 10:17:33 -0500

    clean up templates and views

--- feedbacksite/surveys/urls.py
+++ feedbacksite/surveys/urls.py
@@ -5,10 +5,8 @@ from . import views
 app_name = 'surveys'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/',
          views.FeedbackCreate.as_view(success_url="submitted"),
          name='fill'),
-    path('<int:survey_id>/submit/', views.submit, name='submit'),
     path('<int:pk>/submitted/', views.SubmittedView.as_view(), name='submitted'),
 ]
