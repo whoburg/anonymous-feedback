fa4f3dc4f33931f6521c9f5afecc58dcc612d3ea
Warren Hoburg <whoburg@mit.edu>
2018-11-02 17:36:58 -0500

    add submit_feedback view using FeedbackForm model form. Work in progress. We now have two ways to create feedback objects

--- feedbacksite/surveys/views.py
+++ feedbacksite/surveys/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Survey, Question, Feedback
+from .forms import FeedbackForm
 
 
 class IndexView(generic.ListView):
@@ -15,6 +17,22 @@ class FeedbackCreate(generic.edit.CreateView):
     model = Feedback
     fields = '__all__'
     template_name = 'surveys/fill.html'
+
+
+def submit_feedback(request, pk):
+    survey = get_object_or_404(Survey, pk=pk)
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.question_id = 2
+            feedback.recipient_id = 1
+            feedback.save()
+            return redirect('../submitted')
+    else:
+        form = FeedbackForm()
+    return render(request, 'surveys/submit.html',
+                  {'survey': survey, 'form': form})
 
 
 class SubmittedView(generic.DetailView):
