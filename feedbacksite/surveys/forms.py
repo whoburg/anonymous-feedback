bf04938310db65d135b35d97e3a95d15b1332744
Warren Hoburg <whoburg@mit.edu>
2018-11-03 12:37:31 -0500

    view for new FeedbackForm

--- feedbacksite/surveys/forms.py
+++ feedbacksite/surveys/forms.py
@@ -3,7 +3,11 @@ from django import forms
 from .models import Feedback
 
 
-class FeedbackForm(forms.ModelForm):
+class FeedbackModelForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['feedback_text']
+
+
+class FeedbackForm(forms.Form):
+    your_feedback = forms.CharField(label='Your feedback', max_length=100)
