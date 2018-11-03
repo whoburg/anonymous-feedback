532fdcd1322c547c49924aeb28f74d0afe2f0a8b
Warren Hoburg <whoburg@mit.edu>
2018-11-03 16:38:37 -0500

    first version where feedback forms link to correct question instances

--- feedbacksite/surveys/forms.py
+++ feedbacksite/surveys/forms.py
@@ -8,6 +8,11 @@ class FeedbackModelForm(forms.ModelForm):
         model = Feedback
         fields = ['feedback_text']
 
+    def __init__(self, *args, question, **kwargs):
+        self.question = question
+        super(FeedbackModelForm, self).__init__(*args, **kwargs)
+        self.fields['feedback_text'].label = question.question_text
+
 
 class FeedbackForm(forms.Form):
     your_feedback = forms.CharField(label='Your feedback', max_length=100)
