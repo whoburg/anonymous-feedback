bb5bbf761ecc1119dbbccd48e77ddfb8bfe23372
Warren Hoburg <whoburg@mit.edu>
2018-11-03 20:13:26 -0500

    filter results by survey; all tests pass

--- feedbacksite/surveys/tests.py
+++ feedbacksite/surveys/tests.py
@@ -14,8 +14,16 @@ class TestResultsView(TestCase):
         self.assertTrue(flag)
         self.client.force_login(self.testuser)
 
+    def test_no_survey(self):
+        """"If no survey, should get a 404"""
+        url = reverse('surveys:results', args=(20,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
     def test_no_results(self):
         """"If no results, an appropriate message is displayed"""
+        s = Survey.objects.create(survey_text="Test Survey",
+                                  pub_date=timezone.now())
         url = reverse('surveys:results', args=(1,))
         response = self.client.get(url)
         self.assertContains(response, "No results are available.")
@@ -35,13 +43,13 @@ class TestResultsView(TestCase):
             ['<Feedback: Private feedback for testuser>']
         )
         # now increment the URL pk, there should be no results here
+        # even though a Survey 2 exists
+        Survey.objects.create(survey_text="Test Survey 2",
+                                  pub_date=timezone.now())
         url = reverse('surveys:results', args=(2,))
         response = self.client.get(url)
-        self.assertQuerysetEqual(
-            response.context['feedback_list'],
-            []
-        )
-        print(response.content)
+        self.assertContains(response, "No results are available.")
+        self.assertQuerysetEqual(response.context['feedback_list'], [])
 
     def test_logged_out(self):
         """If user logged out, should get redirected"""
