09cfc9c77ca39db2fb9684400bace92aa10d884a
Warren Hoburg <whoburg@mit.edu>
2018-11-04 10:04:01 -0600

    properly retrieve AUTH_USER_MODEL from settings

--- feedbacksite/surveys/models.py
+++ feedbacksite/surveys/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -19,7 +20,8 @@ class Question(models.Model):
 
 class Feedback(models.Model):
     # author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    recipient = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     feedback_text = models.TextField()
     # created_date = models.DateTimeField(default=timezone.now)
