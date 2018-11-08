0cb4740a1e1eb39afb173c08253b6cf6b56e4e8a
Warren Hoburg <whoburg@mit.edu>
2018-11-08 14:12:40 -0600

    turn off debug

--- feedbacksite/feedbacksite/settings.py
+++ feedbacksite/feedbacksite/settings.py
@@ -25,7 +25,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 del f
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
