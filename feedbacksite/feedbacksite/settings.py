f77b157535116b6b967d263fea4c621b63afa428
Warren Hoburg <whoburg@mit.edu>
2018-11-08 14:09:50 -0600

    read secret key from file

--- feedbacksite/feedbacksite/settings.py
+++ feedbacksite/feedbacksite/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')-z3%sv#3ql+)cimzk&^n*vwodvqdy8e^p&btkugue5=ur9j@u'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
+del f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
