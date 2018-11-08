1c16a6d6cf4ae04e88c524c2287ab42d07cd0741
Warren Hoburg <whoburg@mit.edu>
2018-11-08 14:26:47 -0600

    allowed hosts get u/n

--- feedbacksite/feedbacksite/settings.py
+++ feedbacksite/feedbacksite/settings.py
@@ -27,7 +27,7 @@ del f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['%s.pythonanywhere.com' % os.environ["USERNAME"], '127.0.0.1']
 
 
 # Application definition
