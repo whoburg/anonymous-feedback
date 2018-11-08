ca07e4877937abc966064050068eefd32f689c08
Warren Hoburg <whoburg@mit.edu>
2018-11-08 13:45:00 -0600

    roll back changes to wsgi.py, they belong on python anywhere's copy

--- feedbacksite/feedbacksite/wsgi.py
+++ feedbacksite/feedbacksite/wsgi.py
@@ -1,10 +1,16 @@
-import os
-import sys
+"""
+WSGI config for feedbacksite project.
 
-path = os.path.expanduser('~/feedbacksite')
-if path not in sys.path:
-    sys.path.insert(0, path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'feedbacksite.settings'
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedbacksite.settings')
+
+application = get_wsgi_application()
