8541fc0287e58ae857cfecef1cf9f26d8c2e9feb
Warren Hoburg <whoburg@mit.edu>
2018-11-12 18:15:53 -0600

    store feedback author in database

--- feedbacksite/surveys/models.py
+++ feedbacksite/surveys/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
